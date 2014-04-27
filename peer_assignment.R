8e64e9e078af63e7ff857a4fbe9ada797abdd9ae
yberhane2003 <yberhane@online.no>
2014-04-27 21:15:44 +0200

    update_20142704

--- peer_assignment.R
+++ peer_assignment.R
@@ -16,7 +16,7 @@ X_stdmean <- X_rbind[, c("V1","V2","V3","V4","V5","V6","V41","V42","V43","V44","
 X_stdmean_lbl <- X_stdmean
 
 colnames(X_stdmean_lbl) <- c("tBodyAcc-mean-X","tBodyAcc-mean-Y","tBodyAcc-mean-Z","tBodyAcc-std-X","tBodyAcc-std-Y","tBodyAcc-std-Z","tGravityAcc-mean-X","tGravityAcc-mean-Y","tGravityAcc-mean-Z","tGravityAcc-std-X","tGravityAcc-std-Y","tGravityAcc-std-Z","tBodyAccJerk-mean-X","tBodyAccJerk-mean-Y","tBodyAccJerk-mean-Z","tBodyAccJerk-std-X","tBodyAccJerk-std-Y","tBodyAccJerk-std-Z","tBodyGyro-mean-X","tBodyGyro-mean-Y","tBodyGyro-mean-Z","tBodyGyro-std-X","tBodyGyro-std-Y","tBodyGyro-std-Z","tBodyGyroJerk-mean-X","tBodyGyroJerk-mean-Y","tBodyGyroJerk-mean-Z","tBodyGyroJerk-std-X","tBodyGyroJerk-std-Y","tBodyGyroJerk-std-Z","tBodyAccMag-mean","tBodyAccMag-std","tGravityAccMag-mean","tGravityAccMag-std","tBodyAccJerkMag-mean","tBodyAccJerkMag-std","tBodyGyroMag-mean","tBodyGyroMag-std","tBodyGyroJerkMag-mean","tBodyGyroJerkMag-std","fBodyAcc-mean-X","fBodyAcc-mean-Y","fBodyAcc-mean-Z","fBodyAcc-std-X","fBodyAcc-std-Y","fBodyAcc-std-Z","fBodyAcc-meanFreq-X","fBodyAcc-meanFreq-Y","fBodyAcc-meanFreq-Z","fBodyAccJerk-mean-X","fBodyAccJerk-mean-Y","fBodyAccJerk-mean-Z","fBodyAccJerk-std-X","fBodyAccJerk-std-Y","fBodyAccJerk-std-Z","fBodyAccJerk-meanFreq-X","fBodyAccJerk-meanFreq-Y","fBodyAccJerk-meanFreq-Z","fBodyGyro-mean-X","fBodyGyro-mean-Y","fBodyGyro-mean-Z","fBodyGyro-std-X","fBodyGyro-std-Y","fBodyGyro-std-Z","fBodyGyro-meanFreq-X","fBodyGyro-meanFreq-Y","fBodyGyro-meanFreq-Z","fBodyAccMag-mean","fBodyAccMag-std","fBodyAccMag-meanFreq","fBodyBodyAccJerkMag-mean","fBodyBodyAccJerkMag-std","fBodyBodyAccJerkMag-meanFreq","fBodyBodyGyroMag-mean","fBodyBodyGyroMag-std","fBodyBodyGyroMag-meanFreq","fBodyBodyGyroJerkMag-mean","fBodyBodyGyroJerkMag-std","fBodyBodyGyroJerkMag-meanFreq","angletBodyAccMean-gravity","angletBodyAccJerkMean-gravityMean","angletBodyGyroMean-gravityMean","angletBodyGyroJerkMean-gravityMean","angleX-gravityMean","angleY-gravityMean","angleZ-gravityMean")
-> View(X_stdmean_lbl)
+View(X_stdmean_lbl)
 
 
 #import activities information from train and test folders
@@ -30,6 +30,8 @@ activities_rbind <- rbind(activities_train, activities_test)
 #label activity column
 colnames(activities_rbind) <- c("Activities")
 
+Activity <- activities_rbind
+
 #format activity codes
 
 Activity$Activities[Activity$Activities==1] <-"WALKING"
@@ -38,7 +40,7 @@ Activity$Activities[Activity$Activities==3] <-"WALKING_DOWNSTAIRS"
 Activity$Activities[Activity$Activities==4] <-"SITTING"
 Activity$Activities[Activity$Activities==5] <-"STANDING"
 Activity$Activities[Activity$Activities==6] <-"LAYING"
-Activity$Activities[Activity$Activities=="WALKING_UPSTAIR] <-"WALKING_UPSTAIRs"
+
 
 #attach formatted activity table with Data table
 
