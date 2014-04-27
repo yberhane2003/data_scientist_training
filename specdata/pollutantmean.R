b824e16341a9a1af9c4ac2ed5d7cb4f9d50ec62a
yberhane2003 <yberhane@online.no>
2014-04-27 20:22:43 +0200

    update_20142704

---
+++ specdata/pollutantmean.R
@@ -0,0 +1,29 @@
+
+pollutantmean <- function(mydir,elmnt ="nitrate", indice = 1:332){
+      hmpth <- "C:/coursera/data_scientist_training/"
+      trgdir <-paste(hmpth,mydir, sep= "")
+      setwd(trgdir)
+      mylist <- indice
+      myfile_list <- sprintf("%03d.csv", mylist)
+      if (exists("myset")) {rm("myset")}
+      for (file in myfile_list)
+      {
+            if (!exists("myset"))
+            {
+                  myset<-read.table(file, header = TRUE, sep=",")
+            }
+            else{
+                  currentset<-read.table(file, header = TRUE, sep=",")
+                  myset<-rbind(myset, currentset)
+                  rm(currentset)
+                  
+            }
+            
+      }
+      ##myset <-myset[,myset$eval(elmnt)]
+      myset <-as.data.frame(myset[[elmnt]])
+      lapply(myset,na.rm = TRUE, mean)
+      ##lapply(subset(myset, select=c(nitrate)),na.rm = TRUE, mean)          
+      ##return(myset)
+      
+}
